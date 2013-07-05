834e113cf4dc7e8fd7ba9459780b495b4c012c44
Carolyn Cole <cam156@psu.edu>
2013-07-05 09:17:39 -0400

    Ran Generators for sufia to get a working codebase

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Archivesphere::Application.config.secret_token = 'e63c1ad127d7eab42539bdd326a12c33b06e46bc2205c7debf43071181b158c17b25f1ae30e610ab8d4f985e36415b3601242f519053ce89219eddc63e20ac2d'
