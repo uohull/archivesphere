643f751645f0d5edcb4612b4d7574e47cb541027
Carolyn Cole <cam156@psu.edu>
2013-07-10 14:31:06 -0400

    Adding resque configuration

---
+++ script/restart_resque.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Stops and then starts resque-pool in dev env
+
+function anywait {
+    for pid in "$@"; do
+        while kill -0 "$pid"; do
+            sleep 0.5
+        done
+    done
+}
+
+RESQUE_POOL_PIDFILE="$(pwd)/tmp/pids/resque-pool.pid"
+[ -f $RESQUE_POOL_PIDFILE ] && {
+    PID=$(cat $RESQUE_POOL_PIDFILE)
+    kill -2 $PID && anywait $PID
+}
+
+bundle exec resque-pool --daemon --environment development start
