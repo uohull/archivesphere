374a9234e5646bf4f7a0a16c1933be396b056123
Justin Coyne <justin@curationexperts.com>
2013-07-31 08:28:41 -0500

    Added a javascript test

--- spec/features/create_new_accession_spec.rb
+++ spec/features/create_new_accession_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Creating a new accession" do
+describe "Creating a new accession", :js=>true do
   let(:approved_user) { FactoryGirl.create(:approved_user) }
   before do
     # Stub the ability to access the site to avoid an LDAP call
@@ -8,9 +8,10 @@ describe "Creating a new accession" do
       a.can :access, :site
     end
     ApplicationController.any_instance.stub(:current_ability).and_return(ability)
-  end
-  it "should be successful" do
     login_as (approved_user)
+  end
+
+  it "should be successful" do
     visit '/'
     click_on "Create Collection"
     fill_in "Title", with: "A Test Collection"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -52,19 +53,43 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
-  config.include Devise::TestHelpers, :type => :controller
+  config.include Devise::TestHelpers, type: :controller
   config.include Warden::Test::Helpers, type: :feature
-  config.include UserLogin, type: :feature
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each, type: :model) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :controller) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :feature) do
+    Warden.test_mode!
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.after(:each, type: :feature) do
+    Warden.test_reset!
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  
 end
 
 def define_collection (title = 'title', user = 'jilluser', description ='description', collection_num = '123')
