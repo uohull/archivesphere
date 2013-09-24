b6aa3560fb6448f1de63b7c82af069825d74202c
Carolyn Cole <cam156@psu.edu>
2013-09-24 09:08:13 -0400

    Adding open xml office mime types to the list so that derivatives get displayed for them

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -12,7 +12,10 @@ class GenericFile < ActiveFedora::Base
   alias :accessions= :collections=
 
   def self.office_mime_types
-    ['application/msword','application/vnd.ms-excel','application/vnd.ms-powerpoint']
+    ['application/msword','application/vnd.ms-excel','application/vnd.ms-powerpoint',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'application/vnd.openxmlformats-officedocument.presentationml.presentation']
   end
 
 
