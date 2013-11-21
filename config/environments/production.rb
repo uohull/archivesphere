ac36ec67558313e8d447cfe3631a8b31f534b0e2
Carolyn Cole <cam156@psu.edu>
2013-11-21 13:39:19 -0500

    Adding configuration key into production environment for mailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ Archivesphere::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
