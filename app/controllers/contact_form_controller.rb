fdc554196a221ba507e546ee3f129a197ebd39b4
Carolyn Cole <cam156@psu.edu>
2013-11-21 15:15:20 -0500

    changing response typo. refs #8929

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -7,7 +7,7 @@ class ContactFormController < ApplicationController
   def set_cache_buster
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
-    resonse.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 
 end 
